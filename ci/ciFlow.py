c12a64d0eecdc74b8f1de905a7c176e829df8b11
Oliber Bruendler <oliver.bruendler@psi.ch>
2018-01-26 13:59:41 +0100

    Added check for unexpected Modelsim exit in ciFlow.py

--- ci/ciFlow.py
+++ ci/ciFlow.py
@@ -10,8 +10,15 @@ os.system("vsim -c -do ci.do")
 with open("Transcript.transcript") as f:
 	content = f.read()
 	
+#Expected Errors
 if "###ERROR###" in content:
 	exit(-1)
+#Unexpected Errors
+if "SIMULATIONS COMPLETED SUCCESSFULLY" not in content:
+	exit(-2)
+#Success
+else:
+	exit(0)
 
 ##################################
 # Python Unit Test
