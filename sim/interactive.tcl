1ca4b52fb14474a549353d320c892d8e0c09a243
Oliver Bruendler <oliver.bruendler@psi.ch>
2018-06-05 16:25:56 +0200

    FEATURE: Added TCL script for interactively working from Modelsim TCL console

---
+++ sim/interactive.tcl
@@ -0,0 +1,12 @@
+#This script setps up Modelsim for interactively
+# .. working from the TCL console.
+
+#Import TCL Framework
+source ../../../TCL/PsiSim/PsiSim.tcl
+namespace import psi::sim::*
+
+#Initialize Simulation
+init
+
+#Configure
+source ./config.tcl
